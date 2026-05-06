a358e1b861863805cf771670c96afdde38350958
Drona <drona8978@gmail.com>
2026-05-07 00:21:48 +0530

    Comment out production route handling for frontend files

--- backend/index.js
+++ backend/index.js
@@ -30,18 +30,19 @@ app.use('/api/razorpay', razorpayRoutes);
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
+/*
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..','frontend','dist')));
 
-  app.get(/.*/, (req, res) =>
+  app.get(./*, (req, res) =>
     res.sendFile(path.resolve(__dirname,'..', 'frontend', 'dist', 'index.html'))
   );
-} else {
+} 
+*/
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
-}
+
 
 app.use(notFound);
 app.use(errorHandler);
